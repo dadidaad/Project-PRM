9106a99549aa02941f97cca98d942d5defb901b1
Doan Anh Tu <96458536+TuDoanAnh@users.noreply.github.com>
2022-10-23 10:55:40 +0700

    Update Bottom Navigation bar bằng cơ chế Fragment
    Update Bottom Navigation bar bằng cơ chế Fragment

--- app/src/main/java/com/example/projectprm/view/activities/MainActivity.java
+++ app/src/main/java/com/example/projectprm/view/activities/MainActivity.java
@@ -1,16 +1,82 @@
 package com.example.projectprm.view.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.projectprm.R;
+import com.example.projectprm.view.adapter.ViewPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager mViewPager;
+    private BottomNavigationView mBottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = findViewById(R.id.viewPager);
+        mBottomNavigationView = findViewById(R.id.bottonNav);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        mViewPager.setAdapter(viewPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        mBottomNavigationView.getMenu().findItem(R.id.homeFragment).setChecked(true);
+                        break;
+                    case 1:
+                        mBottomNavigationView.getMenu().findItem(R.id.cartFragment).setChecked(true);
+                        break;
+                    case 2:
+                        mBottomNavigationView.getMenu().findItem(R.id.notiFragment).setChecked(true);
+                        break;
+                    case 3:
+                        mBottomNavigationView.getMenu().findItem(R.id.profileFragment).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.homeFragment:
+                        mViewPager.setCurrentItem(0);
+                        break;
+                    case R.id.cartFragment:
+                        mViewPager.setCurrentItem(1);
+                        break;
+                    case R.id.notiFragment:
+                        mViewPager.setCurrentItem(2);
+                        break;
+                    case R.id.profileFragment:
+                        mViewPager.setCurrentItem(3);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/projectprm/view/adapter/ViewPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.projectprm.view.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.projectprm.view.fragment.CartFragment;
+import com.example.projectprm.view.fragment.HomeFragment;
+import com.example.projectprm.view.fragment.NotiFragment;
+import com.example.projectprm.view.fragment.ProfileFragment;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new CartFragment();
+            case 2:
+                return new NotiFragment();
+            case 3:
+                return new ProfileFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
